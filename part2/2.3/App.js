7ecdd9e4c31dc57b081a1988b355d299a9fa256f
Khyathi-Komati <74000118+Khyathi-Komati@users.noreply.github.com>
2021-06-02 22:28:53 +0530

    Add files via upload

---
+++ part2/2.3/App.js
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id:1,
+    parts: [
+      {
+        id:1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id:2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id:3,
+        name: 'State of a component',
+        exercises: 14
+      },
+      {
+        id:4,
+        name: 'Redux',
+        exercises: 11
+      },
+
+    ]
+  }
+
+  return (
+    <div>
+    < Course a={course}/>
+    </div>
+  )
+}
+const Course = ({a})=>
+{
+  return(
+    <div>
+    <Header b={a.name}/>
+    <Content c={a.parts}/>
+    <Total c={a.parts}/>
+
+    </div>
+  )
+}
+
+const Header=({b})=>{
+  return(
+    <h2>{b}</h2>
+  )
+}
+
+const Content=({c})=>{
+  return(
+    <div>
+      {
+        c.map(d=>{
+          return(
+            <Part key={d.id} e={d.name} f={d.exercises}></Part>
+          )
+        }
+        )
+      }
+
+    </div> 
+  )
+    }
+  const Part=({e,f})=>{
+    return(
+      <p>{e} {f}</p>
+    )
+  }
+
+  const Total=({c})=> {
+    const k=c.map((y)=>{return(y.exercises)})
+    const sum=k.reduce((a,c)=>{return a+c})
+    return(
+      <strong><p>total of{sum} exercises</p></strong>
+    )
+  }
+export default App
